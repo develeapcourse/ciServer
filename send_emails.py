9325a068621e9e4d5afc37b19282774695e16461
Adam Yodinsky <adam.promo@gmail.com>
2018-11-25 16:28:47 +0200

    send mail script refined

--- send_emails.py
+++ send_emails.py
@@ -2,41 +2,29 @@
 import sys
 import smtplib
 
-
+# get arguments
 try:
-	msg = sys.argv[1]
+	dist_list = sys.argv[1]
+	msg = sys.argv[2]
 except Exception as e:
 	print("argument is missing.")
 	print(e)
 	exit(1)
 
-
+dist_list = dist_list.split()
 sender = 'develeapcourse@gmail.com'
 
-dist_list = ['orlykul@gmail.com',
-			'lonnychetrit@gmail.com',
-			'Bittanraphael@gmail.com',
-			'zvitam@hotmail.com',
-			'michael.52@msn.com',
-			'adam.promo@gmail.com',
-			'danny.konstadt@gmail.com',
-			'moria8035@gmail.com']
-
-
+#send email 
+print("conecting gmail...")
 try:
-   server = smtplib.SMTP("smtp.gmail.com", 587)
-   server.connect("smtp.gmail.com", 587)
-   print(1)
-   server.ehlo()
-   server.starttls()
-   print(2)
-   server.login(sender, 'deve1234')
-   print(3)
-   print ("Successfully sent email")
+	server = smtplib.SMTP("smtp.gmail.com", 587)
+	server.connect("smtp.gmail.com", 587)
+	server.ehlo()
+	server.starttls()
+	server.login(sender, 'deve1234')
+	print("sending..")
+	server.sendmail(sender, dist_list, msg)
+	print ("Successfully sent email")
 except Exception as e:
    print ("Error: unable to send email")
    print(e)
-   exit(1)
-
-server.sendmail(sender, dist_list, msg)
-
