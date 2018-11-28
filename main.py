589b8fb0b98ca397cb743e2bf533114958db9325
Danny Konstadt <danny.konstadt@gmail.com>
2018-11-28 10:41:55 +0200

    Email intgration, Move configuration to dictionary and structual changes

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python3
+
 import json
 import requests
 import subprocess
 import shutil
 import tarfile
-import os
+import os.path
+import send_emails
 
+
+from pathlib import Path
 from config_build_run import Config
 from logger import  ci_logger
 
@@ -14,11 +19,12 @@ def main():
 
     ci_logger.start_cycle()
 
-    api_url_base, build_dir, devops_build_script, git_repo, last_run_file, project_dir = get_configuration()
-    last_run_date, last_run_commit  = init_run(project_dir, last_run_file)
+    app_config = get_configuration()
+
+    last_run_date, last_run_commit  = init_run(app_config)
     print ('test last run = ' + str(last_run_date) + " - " + str(last_run_commit))
 
-    current_run_date, current_run_commit = get_commit_info(api_url_base, last_run_date)
+    current_run_date, current_run_commit = get_commit_info(app_config, last_run_date)
     print('test current run = ' + str(current_run_date) + ' - ' + str(current_run_commit))
 
     if current_run_commit is None:
@@ -29,47 +35,58 @@ def main():
     if current_run_commit != last_run_commit:
 
         try:
+            if not os.path.exists(app_config['build_dir']):
+                os.makedirs(app_config['build_dir'])
 
-            if not os.path.exists(build_dir):
-                os.makedirs(build_dir)
-
-            perform_clone_and_build(build_dir, current_run_commit, devops_build_script, git_repo, project_dir)
-            save_current_run(current_run_date, current_run_commit, project_dir, last_run_file)
+            perform_clone_and_build(app_config, current_run_commit)
+            save_current_run(app_config, current_run_date, current_run_commit)
 
         except Exception as e:
             print(e)
 
 def get_configuration():
-    config = Config('.app_config')
 
-    api_url_base = config.get('Git', 'api_url_base')
-    project_dir = config.get('Paths', 'project_dir')
-    git_repo = config.get('Git', 'git_repo')
-    last_run_file = config.get('Project', 'last_run_file')
-    build_dir = config.get('Paths', 'build_dir')
-    devops_build_script = config.get('Paths', 'devops_build_script')
+    app_config_file = Path('.app_config')
 
-    print("test url base = " + str(api_url_base))
-    print("test project_dir = " + str(project_dir))
-    print("test git_repo = " + str(git_repo))
-    print("test last_run_file = " + str(last_run_file))
+    # doesn't exist
+    if not app_config_file.is_file():
+        print("Error! - configuration file not found")
+        exit
 
-    return api_url_base, build_dir, devops_build_script, git_repo, last_run_file, project_dir
+    config = Config(app_config_file)
+    app_config = {}
 
-def perform_clone_and_build(build_dir, current_run_commit, devops_build_script, git_repo, project_dir):
+    app_config['api_url_base'] = config.get('Git', 'api_url_base')
+    app_config['project_dir'] = config.get('Paths', 'project_dir')
+    app_config['git_repo'] = config.get('Git', 'git_repo')
+    app_config['last_run_file'] = config.get('Project', 'last_run_file')
+    app_config['init_run_date'] = config.get('Project', 'init_run_date')
+    app_config['build_dir'] = config.get('Paths', 'build_dir')
+    app_config['devops_build_script'] = config.get('Paths', 'devops_build_script')
+    app_config['dist_list'] = config.get('Mail', 'dist_list')
+    app_config['msg_build_failed'] = config.get('Messages', 'msg_build_failed')
+    app_config['msg_missing_config_file'] = config.get('Messages', 'msg_missing_config_file')
+    app_config['msg_build_server_mail_subject'] = config.get('Messages', 'msg_build_server_mail_subject')
+
+    return app_config
+
+def perform_clone_and_build(app_config, current_run_commit):
     # start from clean
     if current_run_commit is None or str(current_run_commit).strip() == '':
         print("Error! - commit cannot be empy")
         exit()
 
-    shutil.rmtree(build_dir)
-    git("clone", git_repo, build_dir)
-    subprocess.call([devops_build_script, current_run_commit, build_dir])
+    shutil.rmtree(app_config['build_dir'])
+    git("clone", app_config['git_repo'], app_config['build_dir'])
+    status = subprocess.call([app_config['devops_build_script'], current_run_commit, app_config['build_dir']])
+    if status !=  0:
+        send_emails.send(app_config['dist_list'], app_config['msg_build_server_mail_subject'], app_config['msg_build_failed'])
+        exit()
 
-def init_run(project_dir, last_run_file):
+def init_run(app_config):
 
     try:
-        last_run_file = project_dir + "/" + last_run_file
+        last_run_file = app_config['project_dir'] + "/" + app_config['last_run_file']
         last_run = open(last_run_file, "rt")
         last_run_date = last_run.readline().strip('\n')
         print("test last run date - " + str(last_run_date))
@@ -80,7 +97,7 @@ def init_run(project_dir, last_run_file):
 
     except Exception as e:
         # first run values
-        last_run_date = '2018-11-24T00:00:00Z'
+        last_run_date = app_config['init_run_date']#'2018-11-24T00:00:00Z'
         last_run_commit = ''
 
     return last_run_date, last_run_commit
@@ -98,7 +115,7 @@ def archive_artifacts(current_run_date, project_dir, project_name):
 def git(*args):
     return subprocess.check_call(['git'] + list(args))
 
-def save_current_run(current_run_date, current_run_commit, project_dir, last_run_file):
+def save_current_run(app_config, current_run_date, current_run_commit):
 
     try:
         if current_run_date is None or str(current_run_date).strip() == '':
@@ -108,7 +125,7 @@ def save_current_run(current_run_date, current_run_commit, project_dir, last_run
         print("test current run date - " + str(current_run_date))
         print("test current run commit - " + str(current_run_commit))
 
-        current_run_file = project_dir + "/" + last_run_file
+        current_run_file = app_config['project_dir'] + "/" + app_config['last_run_file']
         current_run = open(current_run_file, "wt")
         current_run.truncate(0)  # clean the file
         current_run.write(current_run_date + '\n')
@@ -120,8 +137,8 @@ def save_current_run(current_run_date, current_run_commit, project_dir, last_run
 
         print(e)
 
-def get_commit_info(api_url_base, last_run_date):
-    api_url = "%s?since=%s" % (api_url_base,last_run_date)
+def get_commit_info(app_config, last_run_date):
+    api_url = "%s?since=%s" % (app_config['api_url_base'],last_run_date)
     print ('api url = ' + str(api_url))
     response = requests.get(api_url)
 
--- send_emails.py
+++ send_emails.py
@@ -2,29 +2,28 @@
 import sys
 import smtplib
 
-# get arguments
-try:
-	dist_list = sys.argv[1]
-	msg = sys.argv[2]
-except Exception as e:
-	print("argument is missing.")
-	print(e)
-	exit(1)
+def send(dist_list, subject, msg):
 
-dist_list = dist_list.split()
-sender = 'develeapcourse@gmail.com'
+	if dist_list is None or msg is None:
+		print("argument is missing.")
+		print(e)
+		exit(1)
 
-#send email 
-print("conecting gmail...")
-try:
-	server = smtplib.SMTP("smtp.gmail.com", 587)
-	server.connect("smtp.gmail.com", 587)
-	server.ehlo()
-	server.starttls()
-	server.login(sender, 'deve1234')
-	print("sending..")
-	server.sendmail(sender, dist_list, msg)
-	print ("Successfully sent email")
-except Exception as e:
-   print ("Error: unable to send email")
-   print(e)
+	dist_list = dist_list.split()
+	sender = 'develeapcourse@gmail.com'
+
+	#send email
+	print("conecting gmail...")
+	try:
+		server = smtplib.SMTP("smtp.gmail.com", 587)
+		server.connect("smtp.gmail.com", 587)
+		server.ehlo()
+		server.starttls()
+		server.login(sender, 'deve1234')
+		print("sending..")
+		message = 'Subject: {}\n\n{}'.format(subject, msg)
+		server.sendmail(sender, dist_list, message)
+		print ("Successfully sent email")
+	except Exception as e:
+	   print ("Error: unable to send email")
+	   print(e)
