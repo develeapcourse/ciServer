18f0bbef40831847af1f3a58d3176c45d1df7c90
Danny Konstadt <danny.konstadt@gmail.com>
2018-11-29 09:48:42 +0200

    Always save last commit even in case build failed, Some structual changes

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ import json
 import requests
 import subprocess
 import shutil
-import tarfile
 import os.path
 import send_emails
-
 
 from pathlib import Path
 from config_build_run import Config
@@ -22,10 +20,10 @@ def main():
     app_config = get_configuration()
 
     last_run_date, last_run_commit  = init_run(app_config)
-    print ('test last run = ' + str(last_run_date) + " - " + str(last_run_commit))
+    print ('This run will check commits after = ' + str(last_run_date) + " - " + str(last_run_commit))
 
     current_run_date, current_run_commit = get_commit_info(app_config, last_run_date)
-    print('test current run = ' + str(current_run_date) + ' - ' + str(current_run_commit))
+    print('Latest commit in origin = ' + str(current_run_date) + ' - ' + str(current_run_commit))
 
     if current_run_commit is None:
 
@@ -33,16 +31,27 @@ def main():
         exit()
 
     if current_run_commit != last_run_commit:
+        execute_build(app_config, current_run_commit)
+        save_current_run(app_config, current_run_date, current_run_commit)
 
-        try:
-            if not os.path.exists(app_config['build_dir']):
-                os.makedirs(app_config['build_dir'])
 
-            perform_clone_and_build(app_config, current_run_commit)
-            save_current_run(app_config, current_run_date, current_run_commit)
+def execute_build(app_config, current_run_commit):
+    try:
+        if not os.path.exists(app_config['build_dir']):
+            os.makedirs(app_config['build_dir'])
 
-        except Exception as e:
-            print(e)
+        status = perform_clone_and_build(app_config, current_run_commit)
+
+        if status != 0:
+            print("Error in build")
+
+            dist_list = str(app_config['dist_list']).split()
+            send_emails.send(dist_list, app_config['msg_build_server_mail_subject'],
+                             app_config['msg_build_failed'])
+
+    except Exception as e:
+        print(e)
+
 
 def get_configuration():
 
@@ -54,8 +63,9 @@ def get_configuration():
         exit
 
     config = Config(app_config_file)
-    app_config = {}
 
+    # populate config parameters into dictionary
+    app_config = {}
     app_config['api_url_base'] = config.get('Git', 'api_url_base')
     app_config['project_dir'] = config.get('Paths', 'project_dir')
     app_config['git_repo'] = config.get('Git', 'git_repo')
@@ -71,17 +81,18 @@ def get_configuration():
     return app_config
 
 def perform_clone_and_build(app_config, current_run_commit):
-    # start from clean
+
     if current_run_commit is None or str(current_run_commit).strip() == '':
         print("Error! - commit cannot be empy")
-        exit()
+        return 11
 
+    # start from clean
     shutil.rmtree(app_config['build_dir'])
     git("clone", app_config['git_repo'], app_config['build_dir'])
     status = subprocess.call([app_config['devops_build_script'], current_run_commit, app_config['build_dir']])
-    if status !=  0:
-        send_emails.send(app_config['dist_list'], app_config['msg_build_server_mail_subject'], app_config['msg_build_failed'])
-        exit()
+    print("Return status from build script is: " + str(status))
+
+    return status
 
 def init_run(app_config):
 
@@ -89,34 +100,22 @@ def init_run(app_config):
         last_run_file = app_config['project_dir'] + "/" + app_config['last_run_file']
         last_run = open(last_run_file, "rt")
         last_run_date = last_run.readline().strip('\n')
-        print("test last run date - " + str(last_run_date))
         last_run_commit = last_run.readline().strip('\n')
-        print("test last run commit - " + str(last_run_commit))
 
         last_run.close()
 
     except Exception as e:
         # first run values
-        last_run_date = app_config['init_run_date']#'2018-11-24T00:00:00Z'
+        last_run_date = app_config['init_run_date']
         last_run_commit = ''
 
     return last_run_date, last_run_commit
-
-#TODO - delete
-def archive_artifacts(current_run_date, project_dir, project_name):
-    archives_dir = project_dir + "/archives/"
-    tar_name = archives_dir + project_name + "-" + current_run_date
-    tar = tarfile.open(tar_name, "w")
-
-    for name in ["artifact1", "artifact2", "artifact2"]:
-        tar.add(name)
-        tar.close()
 
 def git(*args):
     return subprocess.check_call(['git'] + list(args))
 
 def save_current_run(app_config, current_run_date, current_run_commit):
-
+    print("Saving current commit")
     try:
         if current_run_date is None or str(current_run_date).strip() == '':
             print("Error! - commit date cannot be empy")
@@ -139,10 +138,11 @@ def save_current_run(app_config, current_run_date, current_run_commit):
 
 def get_commit_info(app_config, last_run_date):
     api_url = "%s?since=%s" % (app_config['api_url_base'],last_run_date)
-    print ('api url = ' + str(api_url))
+    print ('Url for accessing origin repo = ' + str(api_url))
     response = requests.get(api_url)
 
     if response.status_code != 200:
+        print("Error getting current commit from github")
         return None
 
     json_content = json.loads(response.content.decode('utf-8'))
